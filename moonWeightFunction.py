80d7fcac1e3276ede861ffaa93220a5762a34c0b
Diztenz <cosmoc05@yahoo.com>
2021-05-29 15:56:04 -0700

    Added 3rd parameter years

--- moonWeightFunction.py
+++ moonWeightFunction.py
@@ -1,10 +1,10 @@
 # Write your code here :-)
-def moonWeight(weight, increase):
-    for year in range(1, 16):
+def moonWeight(weight, increase, years):
+    for year in range(1, years): # 25 years is actually from 1-26 last number in range does not get added.
         weight = weight + increase
         moonWeight = weight * 0.165
         print('Year %s is %s' % (year, moonWeight))
 
-moonWeight(109, .25)
+
 
 
